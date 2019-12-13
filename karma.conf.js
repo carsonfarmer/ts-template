c3438e2d9924e82bf33f5174efdb531fbd954f5a
Carson Farmer <carson.farmer@gmail.com>
2019-12-12 16:22:21 -0800

    test: karma + mocha + chai + node + browser = :)
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,35 @@
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-    frameworks: ['jasmine', 'karma-typescript'],
-    files: [{ pattern: 'src/**/*.ts' }],
+    frameworks: ["mocha", "chai", "karma-typescript"],
+    files: [
+      "src/**/*.ts"
+    ],
+    exclude: [
+      "src/@types/**"
+    ],
     preprocessors: {
-      '**/*.ts': ['karma-typescript'],
+      "**/*.ts": ["karma-typescript"]
     },
-    reporters: ['mocha', 'karma-typescript'],
-    browsers: ['PhantomJS'],
+    reporters: ["progress", "karma-typescript", "dots"],
+    browsers: ["ChromeHeadless"],
     phantomJsLauncher: {
       exitOnResourceError: true,
     },
-    port: 9876,
-    autoWatch: false,
     singleRun: true,
     karmaTypescriptConfig: {
+      // compilerOptions: {
+      // module: "commonjs",
+      // },
+      tsconfig: "./tsconfig.json",
       reports: {
         html: 'coverage',
-        'text-summary': '', // destination "" will redirect output to the console
+        'text-summary': '', // Destination "" will redirect output to the console
       },
+      bundlerOptions: {
+        acornOptions: {
+          ecmaVersion: 9,
+        },
+      }
     },
-    karmaTypescriptConfig: {
-      disableCodeCoverageInstrumentation: true,
-    },
-  })
-}
+  });
+};
